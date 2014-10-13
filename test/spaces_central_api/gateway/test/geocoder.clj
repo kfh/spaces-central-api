ab35b54e1032b06df3e5870b5f60ccbeed046913
kfh <kfh@kfh.no>
2014-10-13 10:28:16 +0200

    better formatting

--- test/spaces_central_api/gateway/test/geocoder.clj
+++ test/spaces_central_api/gateway/test/geocoder.clj
@@ -1,20 +1,20 @@
 (ns spaces-central-api.gateway.test.geocoder
-  (:require [clojure.test :refer [deftest testing is]]
-            [com.stuartsierra.component :as component]
-            [spaces-central-api.system :refer [spaces-test-geocoder]]
-            [spaces-central-api.gateway.geocoder :refer [geocode-address geocode-location]]))
+    (:require [clojure.test :refer [deftest testing is]]
+              [com.stuartsierra.component :as component]
+              [spaces-central-api.system :refer [spaces-test-geocoder]]
+              [spaces-central-api.gateway.geocoder :refer [geocode-address geocode-location]]))
 
 (deftest geocode-an-address
   (let [system (component/start (spaces-test-geocoder))
         {:keys [geocoder]} system]
     (try
       (testing "Geocoding an address"
-        (let [address {:loc-street "Sukhumvit Road" :loc-street-num "413" :loc-zip-code "10110" :loc-city "Bangkok"}  
-              result (-> (geocode-address (:type geocoder) address) first)
-              {:keys [lng lat]} (-> result :geometry :location)]
-          (is (= (:formatted-address result) "413 Sukhumvit Road, Khlong Toei, Khlong Toei, Bangkok 10110, Thailand"))  
-          (is (= lng 100.5619993))
-          (is (= lat 13.7360241))))  
+               (let [address {:loc-street "Sukhumvit Road" :loc-street-num "413" :loc-zip-code "10110" :loc-city "Bangkok"}  
+                     result (-> (geocode-address (:type geocoder) address) first)
+                     {:keys [lng lat]} (-> result :geometry :location)]
+                 (is (= (:formatted-address result) "413 Sukhumvit Road, Khlong Toei, Khlong Toei, Bangkok 10110, Thailand"))  
+                 (is (= lng 100.5619993))
+                 (is (= lat 13.7360241))))  
       (finally
         (component/stop system)))))
 
@@ -23,11 +23,11 @@
         {:keys [geocoder]} system]
     (try
       (testing "Geocoding a location"
-        (let [location {:long 100.5646072 :lat 13.7341553}  
-              result (-> (geocode-location (:type geocoder) location) first)
-              {:keys [lng lat]} (-> result :geometry :location)]
-          (is (= lng 100.5646072))
-          (is (= lat 13.7341553))
-          (is (= (:formatted-address result) "14-493 Sukhumvit Road, Khlong Toei, Khlong Toei, Bangkok 10110, Thailand"))))
+               (let [location {:long 100.5646072 :lat 13.7341553}  
+                     result (-> (geocode-location (:type geocoder) location) first)
+                     {:keys [lng lat]} (-> result :geometry :location)]
+                 (is (= lng 100.5646072))
+                 (is (= lat 13.7341553))
+                 (is (= (:formatted-address result) "14-493 Sukhumvit Road, Khlong Toei, Khlong Toei, Bangkok 10110, Thailand"))))
       (finally
         (component/stop system)))))
