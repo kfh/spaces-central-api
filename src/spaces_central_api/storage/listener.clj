efeba9589ea9d6d2543c2665cd25b6df6064fcff
kfh <kfh@kfh.no>
2014-11-14 14:10:20 +0100

    transactions need to be separated on status(added/retracted) and handled accordingly

--- src/spaces_central_api/storage/listener.clj
+++ src/spaces_central_api/storage/listener.clj
@@ -8,29 +8,53 @@
 
 (timbre/refer-timbre)
 
-(defn- index [search-api-url attrs]
-  (let [geocodes {:lat (:geocode/latitude attrs) :lon (:geocode/longitude attrs)}
-        location {:id (:ad/public-id attrs) :geocodes geocodes}
-        post (partial http/post (str search-api-url "/api/locations"))]
+(defn- store-location [search-api-url location] 
+  (let [post (partial http/post (str search-api-url "/api/locations"))] 
     (manage 
-      (post {:form-params location :content-type :transit+json :as :transit+json})
+      (let [id (:id location)
+            res (post {:form-params location :content-type :transit+json :as :transit+json})]
+        (if (= 201 (:status res))
+          (info "Storage of location(" id ") in external system succeeded")
+          (warn "Storage of location(" id ") in external system failed")))
       (catch Exception ex
-        (warn "External indexing failed: " (.getMessage ex))))))
+        (warn "External storage of location failed: " (.getMessage ex))))))
+
+(defn- delete-location [search-api-url location] 
+  (manage 
+    (let [id (:id location)
+          res (http/delete (str search-api-url "/api/locations/" id))]
+      (if (= 204 (:status res))
+        (info "Deletion of location(" id ") from external system succeeded")
+        (warn "Deletion of location(" id ") from external system failed")))
+    (catch Exception ex
+      (warn "External deletion of location failed: " (.getMessage ex)))))
+
+(defn- contains-keys? [m keys]
+  (apply = (map count [keys (select-keys m keys)])))
+
+(defn- process-txes [search-api-url attrs]
+  (when (contains-keys? attrs [:ad/public-id :geocode/latitude :geocode/longitude])
+    (let [geocodes {:lat (:geocode/latitude attrs) :lon (:geocode/longitude attrs)}
+          location {:id (:ad/public-id attrs) :geocodes geocodes}]
+      (if (:added? attrs)
+        (store-location search-api-url location)
+        (delete-location search-api-url location)))))
 
 (defn ->attr-data [txes]
   (->> (d/q 
-         '[:find ?geocode ?v 
-           :in $ [[?e ?a ?v _ ?added]] 
+         '[:find ?aname ?v ?added
+           :in $ [[?e ?a ?v _ ?added]]
            :where 
            [?e ?a ?v _ ?added]
-           [?a :db/ident ?geocode]] 
+           [?a :db/ident ?aname]]
          (:db-after txes)
          (:tx-data txes))
-       (into {})))
+       (map #(let [[aname val added] %] {aname val :added? added})) 
+       (apply merge)))
 
-(defn- take-and-index [search-api-url txes]
+(defn- take-and-process-txes [search-api-url txes]
   (go-loop []
-     (index search-api-url (->attr-data (<! txes))) 
+     (process-txes search-api-url (->attr-data (<! txes))) 
     (recur)))
 
 (defrecord TxListener [env watcher]
@@ -42,7 +66,7 @@
       this
       (let [txes (chan)
             tx-tap(tap (:tx-listener watcher) txes)]
-        (take-and-index (:search-api-url env) txes)
+        (take-and-process-txes (:search-api-url env) txes)
         (assoc this :tx-tap tx-tap))))
   
   (stop [this]
